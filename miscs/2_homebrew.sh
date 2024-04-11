a2bbe4104091835912aaa27c9e86da677eea4a85
Savan Rupani <72035991+savanrupani-github@users.noreply.github.com>
2024-04-11 17:28:15 -0500

    Update 2_homebrew.sh

--- miscs/2_homebrew.sh
+++ miscs/2_homebrew.sh
@@ -7,3 +7,8 @@ else
     echo "Homebrew is not installed. Installing now..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+echo "Please restart terminal and execute 3_mac_setup_1.sh"
